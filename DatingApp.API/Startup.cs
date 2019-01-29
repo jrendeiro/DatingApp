5016a8944524f2d94c8b389c5bc2414bb4ca14a0
John <rendeiroj@live.com>
2019-01-29 06:39:07 -0500

    End of lect 39

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -39,7 +39,8 @@ namespace DatingApp.API
                      options.TokenValidationParameters = new TokenValidationParameters
                      {
                          ValidateIssuerSigningKey = true,
-                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
                          ValidateIssuer = false,
                          ValidateAudience = false
                      };
